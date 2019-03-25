4119b0d44f716a4a4107717a1300e4cca2d71a29
Maxime <fightmaxime@hotmail.com>
2019-03-25 00:28:31 -0400

    Ajout de Ubuntu 16.04 & 18.04 Corrections avec master Unzip Silencieux. Modifications de phrase. Etc

--- mineweb-install.sh
+++ mineweb-install.sh
@@ -75,7 +75,7 @@ function installQuestions () {
 	echo ""
 	echo ""
 	echo ""
-	echo "Ok, c'était tout ce dont j'avais besoin. Nous sommes prêts à configurer à commencer l'installation."
+	echo "Ok, c'était tout ce dont j'avais besoin. Nous sommes prêts à commencer l'installation."
 	APPROVE_INSTALL=${APPROVE_INSTALL:-n}
 	if [[ $APPROVE_INSTALL =~ n ]]; then
 		read -n1 -r -p "Appuyez sur n'importe quelle touche pour continuer..."
@@ -95,95 +95,184 @@ function installMineWeb () {
 	if [[ "$OS" =~ (debian|ubuntu) ]]; then
 		if [[ "$VERSION_ID" = "8" ]]; then
 		    apt update
-	            apt remove apt-listchanges -y 
-		    apt upgrade -y
-		    apt install -y ca-certificates apt-transport-https dirmngr zip unzip
+			apt remove apt-listchanges -y 
+			apt upgrade -y
+			apt install -y ca-certificates apt-transport-https dirmngr zip unzip
 		    wget https://dev.mysql.com/get/mysql-apt-config_0.8.8-1_all.deb
-	            ls mysql-apt-config_0.8.8-1_all.deb
-	            dpkg -i mysql-apt-config_0.8.8-1_all.deb
-	            apt-key adv --keyserver keys.gnupg.net --recv-keys 8C718D3B5072E1F5
-	            apt update
-	            apt install mysql-server mysql-client -y
-	            systemctl enable mysql && systemctl start mysql
-		    apt install -y apache2
+	        ls mysql-apt-config_0.8.8-1_all.deb
+	        dpkg -i mysql-apt-config_0.8.8-1_all.deb
+	        apt-key adv --keyserver keys.gnupg.net --recv-keys 8C718D3B5072E1F5
+	        apt update
+	        apt install mysql-server mysql-client -y
+	        systemctl enable mysql && systemctl start mysql
+			apt install -y apache2
 		    wget -q https://packages.sury.org/php/apt.gpg -O- | sudo apt-key add -
-	            echo "deb https://packages.sury.org/php/ jessie main" | tee /etc/apt/sources.list.d/php.list
-	            apt update
-	            apt install php7.2 libapache2-mod-php7.2 php7.2-mysql php7.2-curl php7.2-json php7.2-gd php7.2-memcached php7.2-intl php7.2-sqlite3 php7.2-gmp php7.2-geoip php7.2-mbstring php7.2-xml php7.2-zip -y
+	        echo "deb https://packages.sury.org/php/ jessie main" | tee /etc/apt/sources.list.d/php.list
+	        apt update
+	        apt install php7.2 libapache2-mod-php7.2 php7.2-mysql php7.2-curl php7.2-json php7.2-gd php7.2-memcached php7.2-intl php7.2-sqlite3 php7.2-gmp php7.2-geoip php7.2-mbstring php7.2-xml php7.2-zip -y
 		    service apache2 restart
 		    apt install phpmyadmin -y
-		    rm -rf /usr/share/phpmyadmin/
-	            mkdir /usr/share/phpmyadmin/
-		    cd /usr/share/phpmyadmin/
-		    wget https://files.phpmyadmin.net/phpMyAdmin/4.8.5/phpMyAdmin-4.8.5-all-languages.tar.gz
-		    tar xzf phpMyAdmin-4.8.5-all-languages.tar.gz
-		    mv phpMyAdmin-4.8.5-all-languages/* /usr/share/phpmyadmin
-		    rm /usr/share/phpmyadmin/phpMyAdmin-4.8.5-all-languages.tar.gz
-		    rm -rf /usr/share/phpmyadmin/phpMyAdmin-4.8.5-all-languages
+			rm -rf /usr/share/phpmyadmin/
+			mkdir /usr/share/phpmyadmin/
+			cd /usr/share/phpmyadmin/
+			wget https://files.phpmyadmin.net/phpMyAdmin/4.8.5/phpMyAdmin-4.8.5-all-languages.tar.gz
+			tar xzf phpMyAdmin-4.8.5-all-languages.tar.gz
+			mv phpMyAdmin-4.8.5-all-languages/* /usr/share/phpmyadmin
+			rm /usr/share/phpmyadmin/phpMyAdmin-4.8.5-all-languages.tar.gz
+			rm -rf /usr/share/phpmyadmin/phpMyAdmin-4.8.5-all-languages
 		    if ! grep -q "Include /etc/phpmyadmin/apache.conf" /etc/apache2/apache2.conf; then
 		    echo "Include /etc/phpmyadmin/apache.conf" >> /etc/apache2/apache2.conf
-	            fi
+	        fi
 		    a2enmod rewrite
 		    wget http://mineweb.maximemichaud.me/000-default.conf
 		    mv 000-default.conf /etc/apache2/sites-available/
-	            rm -rf 000-default.conf
+	        rm -rf 000-default.conf
 		    service apache2 restart
 		    rm -rf /var/www/html/
-		    wget https://github.com/MineWeb/MineWebCMS/archive/v1.7.0.zip
-		    mv *.zip /var/www/
+		    wget https://github.com/MineWeb/MineWebCMS/archive/master.zip
+		    mv master.zip /var/www/
 		    cd /var/www/
-		    unzip *.zip
-		    rm -rf *.zip
-		    mv MineWebCMS-1.7.0 /var/www/html
-		    chmod -R 777 /var/www/html
+		    unzip -q master
+		    rm -rf master.zip
+		    mv MineWebCMS-master /var/www/html
+            chmod -R 777 /var/www/html
 		fi
 		if [[ "$VERSION_ID" = "9" ]]; then
 		    apt update
-		    apt -y install ca-certificates apt-transport-https dirmngr
+			apt -y install ca-certificates apt-transport-https dirmngr
 		    wget https://dev.mysql.com/get/mysql-apt-config_0.8.8-1_all.deb
-	            ls mysql-apt-config_0.8.8-1_all.deb
-	            dpkg -i mysql-apt-config_0.8.8-1_all.deb
-	            apt install dirmngr -y
-	            apt-key adv --keyserver keys.gnupg.net --recv-keys 8C718D3B5072E1F5
-	            apt update
-	            apt install mysql-server mysql-client -y
-	            systemctl enable mysql && systemctl start mysql
-		    apt-get install -y apache2
+	        ls mysql-apt-config_0.8.8-1_all.deb
+	        dpkg -i mysql-apt-config_0.8.8-1_all.deb
+	        apt install dirmngr -y
+	        apt-key adv --keyserver keys.gnupg.net --recv-keys 8C718D3B5072E1F5
+	        apt update
+	        apt install mysql-server mysql-client -y
+	        systemctl enable mysql && systemctl start mysql
+			apt install -y apache2
 		    wget -q https://packages.sury.org/php/apt.gpg -O- | sudo apt-key add -
-	            echo "deb https://packages.sury.org/php/ stretch main" | sudo tee /etc/apt/sources.list.d/php.list
-	            apt update
-		    #mem-cached et geoip à check
-	            apt install php7.2 libapache2-mod-php7.2 php7.2-mysql php7.2-curl php7.2-json php7.2-gd php7.2-memcached php7.2-intl php7.2-sqlite3 php7.2-gmp php7.2-geoip php7.2-mbstring php7.2-xml php7.2-zip -y
+	        echo "deb https://packages.sury.org/php/ stretch main" | sudo tee /etc/apt/sources.list.d/php.list
+	        apt update
+			#mem-cached et geoip à check
+	        apt install php7.2 libapache2-mod-php7.2 php7.2-mysql php7.2-curl php7.2-json php7.2-gd php7.2-memcached php7.2-intl php7.2-sqlite3 php7.2-gmp php7.2-geoip php7.2-mbstring php7.2-xml php7.2-zip -y
 		    service apache2 restart
 		    apt-get install -y phpmyadmin
-		    rm -rf /usr/share/phpmyadmin/
-		    mkdir /usr/share/phpmyadmin/
-		    cd /usr/share/phpmyadmin/
-		    wget https://files.phpmyadmin.net/phpMyAdmin/4.8.5/phpMyAdmin-4.8.5-all-languages.tar.gz
-	            tar xzf phpMyAdmin-4.8.5-all-languages.tar.gz
-		    mv phpMyAdmin-4.8.5-all-languages/* /usr/share/phpmyadmin
-	            rm /usr/share/phpmyadmin/phpMyAdmin-4.8.5-all-languages.tar.gz
-		    rm -rf /usr/share/phpmyadmin/phpMyAdmin-4.8.5-all-languages
+			rm -rf /usr/share/phpmyadmin/
+			mkdir /usr/share/phpmyadmin/
+			cd /usr/share/phpmyadmin/
+			wget https://files.phpmyadmin.net/phpMyAdmin/4.8.5/phpMyAdmin-4.8.5-all-languages.tar.gz
+			tar xzf phpMyAdmin-4.8.5-all-languages.tar.gz
+			mv phpMyAdmin-4.8.5-all-languages/* /usr/share/phpmyadmin
+			rm /usr/share/phpmyadmin/phpMyAdmin-4.8.5-all-languages.tar.gz
+			rm -rf /usr/share/phpmyadmin/phpMyAdmin-4.8.5-all-languages
 		    if ! grep -q "Include /etc/phpmyadmin/apache.conf" /etc/apache2/apache2.conf; then
 		    echo "Include /etc/phpmyadmin/apache.conf" >> /etc/apache2/apache2.conf
-	            fi
+	        fi
 		    a2enmod rewrite
 		    wget http://mineweb.maximemichaud.me/000-default.conf
 		    mv 000-default.conf /etc/apache2/sites-available/
-	            rm -rf 000-default.conf
+	        rm -rf 000-default.conf
 		    service apache2 restart
 		    apt install zip -y
 		    rm -rf /var/www/html/
-		    wget https://github.com/MineWeb/MineWebCMS/archive/v1.7.0.zip
-		    mv *.zip /var/www/
+		    wget https://github.com/MineWeb/MineWebCMS/archive/master.zip
+		    mv master.zip /var/www/
 		    cd /var/www/
-		    unzip *.zip
-		    rm -rf *.zip
-		    mv MineWebCMS-1.7.0 /var/www/html
-		    chmod -R 777 /var/www/html
+		    unzip -q master
+		    rm -rf master.zip
+		    mv MineWebCMS-master /var/www/html
+            chmod -R 777 /var/www/html
 		fi	
-		if [[ "$VERSION_ID" = "16.04|18.04" ]]; then
-			apt-get update
+		if [[ "$VERSION_ID" = "16.04" ]]; then
+			apt update
+			apt -y install ca-certificates apt-transport-https dirmngr software-properties-common
+		    wget https://dev.mysql.com/get/mysql-apt-config_0.8.8-1_all.deb
+	        ls mysql-apt-config_0.8.8-1_all.deb
+	        dpkg -i mysql-apt-config_0.8.8-1_all.deb
+	        apt install dirmngr -y
+	        apt-key adv --keyserver keys.gnupg.net --recv-keys 8C718D3B5072E1F5
+	        apt update
+	        apt install mysql-server mysql-client -y
+	        systemctl enable mysql && systemctl start mysql
+			add-apt-repository -y ppa:ondrej/apache2
+			apt update
+		    apt-get install -y apache2
+		    add-apt-repository -y ppa:ondrej/php
+	        apt update
+			#mem-cached et geoip à check
+	        apt install php7.2 libapache2-mod-php7.2 php7.2-mysql php7.2-curl php7.2-json php7.2-gd php7.2-memcached php7.2-intl php7.2-sqlite3 php7.2-gmp php7.2-geoip php7.2-mbstring php7.2-xml php7.2-zip -y
+		    service apache2 restart
+		    apt-get install -y phpmyadmin
+			rm -rf /usr/share/phpmyadmin/
+			mkdir /usr/share/phpmyadmin/
+			cd /usr/share/phpmyadmin/
+			wget https://files.phpmyadmin.net/phpMyAdmin/4.8.5/phpMyAdmin-4.8.5-all-languages.tar.gz
+			tar xzf phpMyAdmin-4.8.5-all-languages.tar.gz
+			mv phpMyAdmin-4.8.5-all-languages/* /usr/share/phpmyadmin
+			rm /usr/share/phpmyadmin/phpMyAdmin-4.8.5-all-languages.tar.gz
+			rm -rf /usr/share/phpmyadmin/phpMyAdmin-4.8.5-all-languages
+		    if ! grep -q "Include /etc/phpmyadmin/apache.conf" /etc/apache2/apache2.conf; then
+		    echo "Include /etc/phpmyadmin/apache.conf" >> /etc/apache2/apache2.conf
+	        fi
+		    a2enmod rewrite
+		    wget http://mineweb.maximemichaud.me/000-default.conf
+		    mv 000-default.conf /etc/apache2/sites-available/
+	        rm -rf 000-default.conf
+		    service apache2 restart
+		    apt install zip -y
+		    rm -rf /var/www/html/
+		    wget https://github.com/MineWeb/MineWebCMS/archive/master.zip
+		    mv master.zip /var/www/
+		    cd /var/www/
+		    unzip -q master
+		    rm -rf master.zip
+		    mv MineWebCMS-master /var/www/html
+            chmod -R 777 /var/www/html
+		fi
+		if [[ "$VERSION_ID" = "18.04" ]]; then
+			apt update
+			apt -y install ca-certificates apt-transport-https dirmngr software-properties-common
+		    wget https://dev.mysql.com/get/mysql-apt-config_0.8.8-1_all.deb
+	        ls mysql-apt-config_0.8.8-1_all.deb
+	        dpkg -i mysql-apt-config_0.8.8-1_all.deb
+	        apt install dirmngr -y
+	        apt-key adv --keyserver keys.gnupg.net --recv-keys 8C718D3B5072E1F5
+	        apt update
+	        apt install mysql-server mysql-client -y
+	        systemctl enable mysql && systemctl start mysql
+			add-apt-repository -y ppa:ondrej/apache2
+			apt update
+		    apt-get install -y apache2
+		    add-apt-repository -y ppa:ondrej/php
+	        apt update
+			#mem-cached et geoip à check
+	        apt install php7.2 libapache2-mod-php7.2 php7.2-mysql php7.2-curl php7.2-json php7.2-gd php7.2-memcached php7.2-intl php7.2-sqlite3 php7.2-gmp php7.2-geoip php7.2-mbstring php7.2-xml php7.2-zip -y
+		    service apache2 restart
+		    apt-get install -y phpmyadmin
+			rm -rf /usr/share/phpmyadmin/
+			mkdir /usr/share/phpmyadmin/
+			cd /usr/share/phpmyadmin/
+			wget https://files.phpmyadmin.net/phpMyAdmin/4.8.5/phpMyAdmin-4.8.5-all-languages.tar.gz
+			tar xzf phpMyAdmin-4.8.5-all-languages.tar.gz
+			mv phpMyAdmin-4.8.5-all-languages/* /usr/share/phpmyadmin
+			rm /usr/share/phpmyadmin/phpMyAdmin-4.8.5-all-languages.tar.gz
+			rm -rf /usr/share/phpmyadmin/phpMyAdmin-4.8.5-all-languages
+		    if ! grep -q "Include /etc/phpmyadmin/apache.conf" /etc/apache2/apache2.conf; then
+		    echo "Include /etc/phpmyadmin/apache.conf" >> /etc/apache2/apache2.conf
+	        fi
+		    a2enmod rewrite
+		    wget http://mineweb.maximemichaud.me/000-default.conf
+		    mv 000-default.conf /etc/apache2/sites-available/
+	        rm -rf 000-default.conf
+		    service apache2 restart
+		    apt install zip -y
+		    rm -rf /var/www/html/
+		    wget https://github.com/MineWeb/MineWebCMS/archive/master.zip
+		    mv master.zip /var/www/
+		    cd /var/www/
+		    unzip -q master
+		    rm -rf master.zip
+		    mv MineWebCMS-master /var/www/html
+            chmod -R 777 /var/www/html
 		fi
 	elif [[ "$OS" = 'centos' ]]; then
 		yum install -y epel-release
